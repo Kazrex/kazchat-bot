702e713148b66fe5a26fc8028f05c93f4530a953
Admin <100080527+Kazrex@users.noreply.github.com>
2025-07-16 14:22:07 +0500

    Delete config directory

--- config/config.js
+++
@@ -1,5 +0,0 @@
-// Конфигурация файл
-module.exports = {
-  BOT_TOKEN: process.env.BOT_TOKEN,  // 8024810835:AAFyo-u6nNi43RLIH7AHoAcTQJJwc_K8kxg
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY  // OpenAI API кілті
-};
